71609b07c3be3e82f44aa5438cf2b4f95e487ef7
chollatit1995 <78395665+chollatit1995@users.noreply.github.com>
2023-10-16 17:07:55 +0700

    Example4

---
+++ Exercise4_Chollatit_N.py
@@ -0,0 +1,10 @@
+eng = 80.0
+bus = 70.8
+comsys = 75.6
+compro = 90.0
+
+print("--- Your Score ---")
+print("Foundation English : ", eng, "Point")
+print("General Business :", bus, "Point")
+print("Introduction to Computer Systems :", comsys, "Point")
+print("Computer Programming :", compro, "Point")
