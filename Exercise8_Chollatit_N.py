c913aecc73ebf81c4473c3468f1d7eb219676bcf
chollatit1995 <78395665+chollatit1995@users.noreply.github.com>
2023-10-19 15:39:29 +0700

    update2

--- Exercise8_Chollatit_N.py
+++ Exercise8_Chollatit_N.py
@@ -18,35 +18,48 @@ if username == "admin" and password == "123":
             price = 10
             item = 5 + amount
             totalAmount = amount * price
-            print("Orange :", item, "item")
-            print("Total amount : ", float(totalAmount), "THB")
-            print("---------------------")
+            vat = 7
+            result = totalAmount * vat / 100
+            print("Apple :", item, "item")
+            print("Vat 7 % :", result, "THB")
+            print("Total amount : ", totalAmount+result, "THB")
+            # print("---------------------")
         elif amount <= 10:
             price = 10
             totalAmount = amount * price
-            print("Orange :", amount, "item")
-            print("Total amount : ", totalAmount, "THB")
-            print("---------------------")
+            vat = 7
+            result = totalAmount * vat / 100
+            print("Apple :", amount, "item")
+            print("Vat 7 % :", result, "THB")
+            print("Total amount : ", totalAmount+result, "THB")
+            # print("---------------------")
     elif userSelected == 2:
         print("Promotion Sell 50%")
         amount = int(input("How much do you want :"))
-        print("---------------------")
+        # print("---------------------")
         price = 5
         totalAmount = (amount * price)/2
+        vat = 7
+        result = totalAmount * vat / 100
         print("Banana :", amount, "item")
-        print("Total amount : ", totalAmount, "THB")
-        print("---------------------")
+        print("Vat 7 % :", result, "THB")
+        print("Total amount : ", totalAmount+result, "THB")
+        # print("---------------------")
     elif userSelected == 3:
         print("Promotion Reduced price 5 THB")
         amount = int(input("How much do you want :"))
-        print("---------------------")
         sell = 5
-        price = 15 - 5
+        price = 15 - sell
         totalAmount = amount * price
+        vat = 7
+        result = totalAmount * vat / 100
         print("Orange :", amount, "item")
-        print("Total amount : ", float(totalAmount), "THB")
-        print("---------------------")
+        print("Vat 7 % :", result, "THB")
+        print("Total amount : ", totalAmount+result, "THB")
     else:
         print("There are no items selected.")
+    print("---------------------")
+    print("----- THANK YOU -----")
+    print("---------------------")
 else:
     print("Login Fail !")
