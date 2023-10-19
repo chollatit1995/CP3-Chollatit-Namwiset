90d29918243c256d5e481d5b89d949d470f3bc45
chollatit1995 <78395665+chollatit1995@users.noreply.github.com>
2023-10-19 14:39:31 +0700

    Exercise8

---
+++ Exercise8_Chollatit_N.py
@@ -0,0 +1,50 @@
+username = input("username :")
+password = input("password :")
+
+if username == "admin" and password == "123":
+    print("------ Welcome ------")
+    print("----- inno shop -----")
+    print("1. Apple     10 THB")
+    print("2. Banana    5 THB")
+    print("3. Orange    15 THB")
+    print("---------------------")
+    userSelected = int(input("Choose the desired fruit :"))
+    print("---------------------")
+    if userSelected == 1:
+        print("Promotion 10 Free 5")
+        amount = int(input("How much do you want :"))
+        print("---------------------")
+        if amount == 10:
+            item = 5 + amount
+            price = 10
+            totalAmount = amount * price
+            print("Orange :", item, "item")
+            print("Total amount : ", totalAmount, "THB")
+            print("---------------------")
+        elif amount <= 10:
+            price = 10
+            totalAmount = amount * price
+            print("Orange :", amount, "item")
+            print("Total amount : ", totalAmount, "THB")
+            print("---------------------")
+    elif userSelected == 2:
+        amount = int(input("How much do you want :"))
+        print("---------------------")
+        price = 5
+        totalAmount = amount * price
+        print("Total amount : ", totalAmount)
+        print("---------------------")
+    elif userSelected == 3:
+        print("Promotion Sell 5 THB")
+        amount = int(input("How much do you want :"))
+        print("---------------------")
+        sell = 5
+        price = 15 - 5
+        totalAmount = amount * price
+        print("Orange :", amount, "item")
+        print("Total amount : ", totalAmount, "THB")
+        print("---------------------")
+    else:
+        print("There are no items selected.")
+else:
+    print("Login Fail !")
