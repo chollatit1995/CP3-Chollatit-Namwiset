7b8f72f2a01565b6f1b6febbf0029043a36a832b
chollatit1995 <78395665+chollatit1995@users.noreply.github.com>
2023-11-20 21:48:32 +0700

    20112023

---
+++ FunctionParamExample.py
@@ -0,0 +1,9 @@
+x = 20
+y = 10
+def addNumber(x,y):
+    print(x + y)
+    print(x - y)
+    print(x * y)
+    print(x / y)
+
+addNumber(x,y)
---
+++ NestedLoopExample.py
@@ -0,0 +1,11 @@
+def addNumber():
+    print("Heelo Wolrd")
+    addUser()
+    print("Wooooo!!")
+def addUser():
+    print("Heelo Chollatit")
+    print("Hahaa")
+    addNumber()
+
+addNumber()
+addUser()
